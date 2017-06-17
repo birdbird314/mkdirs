cb956c0e5960930f2011708c708bbca7e42e777e
birdbird314 <grantstadt@gmail.com>
2017-06-17 16:52:25 +0200

    Implement mkdirs for single directory

--- src/mkdirs/core.clj
+++ src/mkdirs/core.clj
@@ -1,6 +1,28 @@
-(ns mkdirs.core)
+(ns mkdirs.core
+  (:require [clojure.string :as str]))
 
-(defn foo
-  "I don't do a whole lot."
-  [x]
-  (println x "Hello, World!"))
+(defn- root? [dir] (= "/" dir))
+
+(defn- dir-exists? 
+  [existing to-add]
+  (or
+    (root? to-add)
+    (some #(str/starts-with? % to-add) existing)))
+
+(defn- parent 
+  [dir] 
+  (let [result 
+         (reduce
+           #(str %1 "/" %2) 
+           (drop-last (str/split dir #"/")))]
+    (if (empty? result) 
+	  "/"
+	  result)))
+
+(defn mkdirs 
+  [existing to-add]
+  (loop [result 0
+         dir (first to-add)]
+    (if (dir-exists? existing dir)
+      result 
+      (recur (inc result) (parent dir)))))
--- test/mkdirs/core_test.clj
+++ test/mkdirs/core_test.clj
@@ -2,6 +2,19 @@
   (:require [clojure.test :refer :all]
             [mkdirs.core :refer :all]))
 
-(deftest a-test
-  (testing "I will never let you down"
-    (is true)))
+(deftest mkdirs-test
+  (testing "Should return 0 for root dir"
+    (is (zero? (mkdirs `() '("/")))))
+    
+  (testing "Should return path depth when there are no predefined dirs "
+    (is (= 2 (mkdirs `() '("/a/a")))))
+  
+  (testing "Should return 0 when directory already exist"
+    (is (zero? (mkdirs `("/a/a") '("/a"))))
+    (is (zero? (mkdirs `("/a/a") '("/")))))
+
+  (testing "Should count number of mkdirs for one dirs"
+    (is (= 2 (mkdirs '("/a") '("/a/b/c"))))) 
+ 
+  (testing "Should count number of mkdirs for multiple dirs"
+    (is (= 4 (mkdirs `("/a" "/b/c") '("/a/b" "/a/c" "/b/c/d/e"))))))
